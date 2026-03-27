205e19f645d30163751bca94667e6a4e02677e12
charantechie <charantechie@gmail.com>
2026-03-27 13:30:20 +0100

    Removed the properties from services and integrated the services to the config server

--- src/main/java/com/banking/cards/dto/CardsContactInfoDto.java
+++ src/main/java/com/banking/cards/dto/CardsContactInfoDto.java
@@ -2,9 +2,10 @@ package com.banking.cards.dto;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = CardsContactInfoDto.PREFIX)
-public record CardsContactInfoDto(String message, Map<String, String> contactDetails, String onCallSupport) {
+public record CardsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
     static final String PREFIX = "cards";
 }
