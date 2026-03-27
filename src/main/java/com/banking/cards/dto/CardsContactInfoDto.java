8b7bb44775c52b6820fc248bd1b8c6e474759c7e
charantechie <charantechie@gmail.com>
2026-03-27 16:07:59 +0100

    Enable Actuator Refresh in services so that dynamic configuration changes are possible without server restart

--- src/main/java/com/banking/cards/dto/CardsContactInfoDto.java
+++ src/main/java/com/banking/cards/dto/CardsContactInfoDto.java
@@ -1,11 +1,18 @@
 package com.banking.cards.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
+@Getter
+@Setter
 @ConfigurationProperties(prefix = CardsContactInfoDto.PREFIX)
-public record CardsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+public class CardsContactInfoDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
     static final String PREFIX = "cards";
 }
